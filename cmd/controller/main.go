0a8ccdff094426c5544403b79ceecc89512fbbcc
Quan Anh Tong <quanta.linux@gmail.com>
2021-04-14 08:25:11 +0700

    implement custom controller

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -1,1 +1,111 @@
-package controller
+package main
+
+import (
+	"os"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	apiextensionsclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
+	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog"
+
+	foov1 "github.com/quantonganh/kubernetes-test-controller/pkg/apis/foo/v1"
+	fooclientset "github.com/quantonganh/kubernetes-test-controller/pkg/client/clientset/versioned"
+	fooscheme "github.com/quantonganh/kubernetes-test-controller/pkg/client/clientset/versioned/scheme"
+	fooinformers "github.com/quantonganh/kubernetes-test-controller/pkg/client/informers/externalversions"
+	foolisters "github.com/quantonganh/kubernetes-test-controller/pkg/client/listers/foo/v1"
+)
+
+type Controller struct {
+	kubeclientset          kubernetes.Interface
+	apiextensionsclientset apiextensionsclientset.Interface
+	clientset              fooclientset.Interface
+	informer               cache.SharedIndexInformer
+	lister                 foolisters.FooLister
+	recorder               record.EventRecorder
+	workqueue              workqueue.RateLimitingInterface
+}
+
+func NewController() *Controller {
+	kubeconfig := os.Getenv("KUBECONFIG")
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		klog.Fatalf(err.Error())
+	}
+
+	kubeClient := kubernetes.NewForConfigOrDie(config)
+	apiextensionsClient := apiextensionsclientset.NewForConfigOrDie(config)
+	testClient := fooclientset.NewForConfigOrDie(config)
+
+	informerFactory := fooinformers.NewSharedInformerFactory(testClient, 1*time.Minute)
+	informer := informerFactory.Foo().V1().Foos()
+	informer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(object interface{}) {
+			klog.Infof("Added: %v", object)
+		},
+		UpdateFunc: func(oldObject, newObject interface{}) {
+			klog.Infof("Updated: %v", newObject)
+		},
+		DeleteFunc: func(object interface{}) {
+			klog.Infof("Deleted: %v", object)
+		},
+	})
+	informerFactory.Start(wait.NeverStop)
+
+	utilruntime.Must(foov1.AddToScheme(fooscheme.Scheme))
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartLogging(klog.Infof)
+	eventBroadcaster.StartRecordingToSink(
+		&typedcorev1.EventSinkImpl{
+			Interface: kubeClient.CoreV1().Events(""),
+		},
+	)
+	recorder := eventBroadcaster.NewRecorder(
+		fooscheme.Scheme,
+		corev1.EventSource{
+			Component: "foo-controller",
+		},
+	)
+
+	workqueue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+	return &Controller{
+		kubeclientset:          kubeClient,
+		apiextensionsclientset: apiextensionsClient,
+		clientset:              testClient,
+		informer:               informer.Informer(),
+		lister:                 informer.Lister(),
+		recorder:               recorder,
+		workqueue:              workqueue,
+	}
+}
+
+func (c *Controller) Run() {
+	defer utilruntime.HandleCrash()
+	defer c.workqueue.ShutDown()
+
+	klog.Infof("Waiting cache to be synced")
+	timeout := time.NewTimer(30 * time.Second)
+	timeoutCh := make(chan struct{})
+	go func() {
+		<-timeout.C
+		timeoutCh <- struct{}{}
+	}()
+	if ok := cache.WaitForCacheSync(timeoutCh, c.informer.HasSynced); !ok {
+		klog.Fatalln("Timeout expired during waiting for caches to sync")
+	}
+	klog.Infoln("Starting custom controller")
+	select {}
+}
+
+func main() {
+	controller := NewController()
+	controller.Run()
+}
